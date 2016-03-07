4edade934baff680ab3b24c34f973d249a001747
Carlos Kozuszko <carlos@insignia4u.com>
2016-03-07 17:19:32 -0300

    Agregando productos en oferta.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,6 @@
 class HomeController < ApplicationController
   def index
+    @latest_products = Product.order('created_at desc').limit(4)
+    @sale_products = Product.where(sale: true).order('created_at desc').limit(6)
   end
 end
